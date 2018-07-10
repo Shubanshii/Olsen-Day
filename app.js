cd74cf586b4dedb4a5d31864d4e57bb2e785abc1
unknown <c_day@alumni.baylor.edu>
2018-07-10 15:48:13 -0500

    modified port var and config.json

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require('path');
 const nodemailer = require('nodemailer');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
